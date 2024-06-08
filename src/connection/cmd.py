6833cdba2e379632dc5c0901da4e1e041d575f80
ljbeal <louis.j.beal@gmail.com>
2024-06-08 15:25:53 +0200

    Implement CMD streaming

--- src/connection/cmd.py
+++ src/connection/cmd.py
@@ -1,46 +1,111 @@
+"""
+Small module to handle an instance of a command execution
+"""
 import subprocess
+import threading
 
 
 class CMD:
+    """
+    Command instance
 
-    __slots__ = ["_cmd", "_subprocess", "_stdout", "_stderr"]
+    Set up the command then exec() the instance
+    """
+
+    __slots__ = ["_cmd", "_stdout", "_stderr", "_returncode"]
 
     def __init__(self, cmd: str):
         self._cmd = cmd
-        self._subprocess = None
 
         self._stdout = None
         self._stderr = None
+        self._returncode = None
 
-    def exec(self) -> None:
-        self._subprocess = subprocess.Popen(
+    def exec(self, passthrough: bool = False) -> None:
+        """
+        Generates a subprocess instance to execute the stored command
+        """
+        with subprocess.Popen(
             self._cmd,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             shell=True,
-            text=True
-        )
+            text=True,
+            executable="bash"
+        ) as proc:
+            stdout_cache = []
+            stdout_thread = threading.Thread(
+                target=stream_capture,
+                args=[proc.stdout, stdout_cache, passthrough]
+            )
+            stderr_cache = []
+            stderr_thread = threading.Thread(
+                target=stream_capture,
+                args=[proc.stderr, stderr_cache, passthrough]
+            )
 
-        self._stdout, self._stderr = self._subprocess.communicate()
+            stdout_thread.start()
+            stderr_thread.start()
+
+            while proc.poll() is None:
+                pass
+
+            stdout_thread.join()
+            stderr_thread.join()
+
+            self._stdout = "".join(stdout_cache)
+            self._stderr = "".join(stderr_cache)
 
     @property
-    def returncode(self):
-        if self._subprocess is None:
-            return None
+    def returncode(self) -> None | int:
+        """Access the returncode of the subprocess"""
 
-        self._subprocess.poll()
-        return self._subprocess.returncode
-
-    @property
-    def finished(self) -> bool:
-        if self._subprocess is None:
-            return False
-        return self.returncode is not None
+        return self._returncode
 
     @property
     def stdout(self) -> None | str:
+        """Returns the captured stdout"""
         return self._stdout
 
     @property
     def stderr(self) -> None | str:
+        """Returns the captured stderr"""
         return self._stderr
+
+
+def stream_capture(pipe, cache: list, passthrough: bool = False) -> None:
+    """
+    Captures the output of a pipe, used with threading to capture simultaneous pipes
+
+    Stores the output in place within the provided cache
+
+    :param pipe:
+        subprocess redirect
+    :param cache:
+        list to cache lines into
+    :param passthrough:
+        prints output as it goes if True, default False
+    :return:
+        None
+    """
+    for line in iter(pipe.readline, ""):
+        pre = ""
+        end = "\n"
+
+        tmp = pre + line.rstrip("\n") + end
+        cache.append(tmp)
+        if passthrough:
+            print(tmp, end="")
+
+
+if __name__ == "__main__":
+    TEST_CMD = """for ((i=0; i < 10; i++)); do
+    echo $i
+    sleep 0.25
+done
+"""
+
+    test = CMD(TEST_CMD)
+    test.exec(passthrough=False)
+
+    print(test.stdout)

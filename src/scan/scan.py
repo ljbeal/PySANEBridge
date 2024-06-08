800673b5562afe8685197483d07f68ad5746af6d
ljbeal <louis.j.beal@gmail.com>
2024-06-08 19:28:54 +0200

    primitive scanning ability

--- src/scan/scan.py
+++ src/scan/scan.py
@@ -1,7 +1,16 @@
+"""
+The scan class holds the actual scanner instance
+"""
+import random
+import shutil
+
 from src.connection.connection import Connection
 
 
 class Scanner:
+    """
+    Class for interacting with the scanner proper
+    """
 
     __slots__ = ["_conn", "_imagecache"]
 
@@ -9,8 +18,30 @@ class Scanner:
 
         self._conn = Connection(userhost=userhost)
 
+    @property
+    def conn(self):
+        """Returns the private connection property"""
+        return self._conn
+
+    def get_devices(self) -> list:
+        """Retrieve a list of available scanners"""
+        devices = [line for line in self.conn.cmd("scanimage -L").stdout.split("\n") if line != ""]
+
+        return devices
+
+    def scan(self, output_name: str = "test.png"):
+        """Scan an image, saving it locally to output_name"""
+        filename = "".join([str(random.randint(0, 9)) for i in range(16)]) + ".png"
+        self.conn.cmd(f"scanimage --format png --output-file {filename} --progress", stream=True)
+
+        self.conn.cmd(f"scp {self.conn.userhost}:{filename} .", local=True)
+
+        self.conn.cmd(f"rm {filename}")
+
+        shutil.move(filename, output_name)
+
 
 if __name__ == "__main__":
-    machine = Connection("pi@192.168.0.8")
+    machine = Scanner("pi@192.168.0.8")
 
-    print(machine.cmd("pwd"))
+    machine.scan()

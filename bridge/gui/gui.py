113b50acf73ed1aec00bfbc8a0f55fac337ccdcf
ljbeal <louis.j.beal@gmail.com>
2024-06-10 18:01:47 +0200

    ugly, but functional GUI

--- bridge/gui/gui.py
+++ bridge/gui/gui.py
@@ -5,8 +5,8 @@ UI module
 import sys
 
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QHBoxLayout, QMenu, QToolBar, QPushButton, \
-    QLineEdit
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QGridLayout, QHBoxLayout, QMenu, QToolBar, QPushButton, \
+    QLineEdit, QDialog
 
 from bridge.gui.settings import Settings
 from bridge.scan.scan import Scanner
@@ -22,12 +22,19 @@ class MainWindow(QMainWindow):
 
         self._settings = Settings("settings.ini")
 
+        self._continue_scanning = True
+        self._current_popup = None
+
         self.setWindowTitle("Scanner SANE Bridge")
         self.setGeometry(100, 100, 600, 400)
 
         self.UISetup()
 
         self.show()
+
+    def close_current_popup(self):
+        if self._current_popup is not None:
+            self._current_popup.close()
 
     @property
     def settings(self):
@@ -71,18 +78,63 @@ class MainWindow(QMainWindow):
 
         scanner = Scanner(userhost)
 
-        continue_scanning = True
+        self._continue_scanning = True
         images = []
-        while continue_scanning:
+        while self._continue_scanning:
             image = scanner.scan_image(resolution=resolution)
 
             images.append(image)
 
-            continue_scanning = False  # popup dialog for continue
+            ask_continue_window = QDialog(self)
+            self._current_popup = ask_continue_window
+            ask_continue_window.setWindowTitle("Continue Scanning?")
 
-        filename = "test.pdf"
+            container = QGridLayout()
 
+            continue_y = QPushButton("Yes")
+            continue_n = QPushButton("No")
+            contmessag = QLabel("Scan Additional Pages?:")
+
+            container.addWidget(contmessag, 0, 0, 1, 2)
+            container.addWidget(continue_n, 1, 0)
+            container.addWidget(continue_y, 1, 1)
+
+            continue_n.clicked.connect(self.set_continue_false)
+            continue_y.clicked.connect(self.set_continue_true)
+
+            ask_continue_window.setLayout(container)
+            ask_continue_window.exec()
+
+        ask_filename = QDialog(self)
+        self._current_popup = ask_filename
+        ask_filename.setWindowTitle("Choose where to save the file")
+
+        # just a filename for now
+        name_entry = QLineEdit()
+        ok_button = QPushButton("Save")
+        ok_button.clicked.connect(self.close_current_popup)
+
+        container = QGridLayout()
+        container.addWidget(name_entry, 0, 0)
+        container.addWidget(ok_button, 0, 1)
+
+        ask_filename.setLayout(container)
+        ask_filename.exec()
+
+        filename = name_entry.text()
+
+        filename = f"{os.path.splittext(filename)}.pdf"  # force pdf
+
+        print(f"Saving image out to {filename}")
         images[0].save(fp=filename, format="PDF", save_all=True, append_images=images[1:])
+
+    def set_continue_true(self):
+        self._continue_scanning = True
+        self.close_current_popup()
+
+    def set_continue_false(self):
+        self._continue_scanning = False
+        self.close_current_popup()
 
 
 if __name__ == "__main__":
